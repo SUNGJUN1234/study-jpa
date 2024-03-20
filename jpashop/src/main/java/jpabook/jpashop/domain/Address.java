9ebe7dc38ce0d1ca831db3fa7f9f14e0b451598b
SUNGJUN1234 <chotjdwns654@gmail.com>
2024-03-20 15:32:28 +0900

    모든 연관관계 EAGER(즉시로딩) 에서 LAZY(지연로딩)로 설정

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,13 +1,22 @@
 package jpabook.jpashop.domain;
 
-import jdk.jfr.Enabled;
+import jakarta.persistence.Embeddable;
 import lombok.Getter;
 
-@Enabled
+@Embeddable
 @Getter
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
